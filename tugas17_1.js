b3f4f3e8a31a1ab91926e547b716529efda6ee53
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-09-21 11:29:22 +0700

    Add files via upload

---
+++ tugas17_1.js
@@ -0,0 +1,5 @@
+paragraf = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+console.log(paragraf.substr(0,6))
+
+console.log(paragraf.charCodeAt(0), " - ", paragraf.charCodeAt(1), " - ", paragraf.charCodeAt(2), " - ", paragraf.charCodeAt(3), " - ", paragraf.charCodeAt(4), " - ", paragraf.charCodeAt(5) )
